8a8c6fe8719440de12ebfcb65792d238beb03af0
Riyansh Singh <itsyourriyansh@gmail.com>
2025-06-04 01:42:41 +0530

    Version 2.0

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
 // Form Tab Switching
 const tabBtns = document.querySelectorAll('.tab-btn');
 const forms = document.querySelectorAll('.form');
